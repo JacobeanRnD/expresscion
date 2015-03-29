dc2e69f2193a18dc5819134e0b99b8c0faa50b89
Feyzo <feyzullahdaldal@gmail.com>
2015-03-29 15:01:51 +0300

    Fixed layout and window resize

--- public/main.js
+++ public/main.js
@@ -14,7 +14,13 @@ $(function() {
   var vizArea = $('#viz-area'),
     layout,
     eventChangeSource,
-    scxmlChangeSource;
+    scxmlChangeSource,
+    isFirst = true;
+
+  var updateLayout = _.debounce(function(e) {
+    layout.invalidateSize();
+  }, 500);
+  window.addEventListener("resize", updateLayout, false);
 
   getScxml();
 
@@ -24,94 +30,44 @@ $(function() {
         url: statechartUrl,
         dataType: 'text'
       })
-      .done(function(data, status, xhr) {
+      .done(function(content, status, xhr) {
         if (status !== 'success') {
           alert('Error retrieving scxml content:', status);
           console.log(xhr);
           return;
         }
-        drawSimulation(data);
-      });
-  }
+        
+        drawSimulation(content, function () {
+          if(isFirst) {
+            layout.fit();
+            isFirst = false;  
+          }
 
-  function highlight(eventName, state) {
-    if (Array.isArray(state)) {
-      for (var eventIndex in state) {
-        layout.highlightState(state[eventIndex], eventName === 'onEntry');
-      }
-    } else {
-      layout.highlightState(state, eventName === 'onEntry');
-    }
-  }
+          if(!scxmlChangeSource) {
+            scxmlChangeSource = new EventSource(statechartChangesUrl);
 
-  function drawSimulation(content) {
-    try {
-      var doc = (new DOMParser()).parseFromString(content, 'application/xml');
-      if (doc.getElementsByTagName('parsererror').length) {
-        throw ({
-          //Only div in parsererror contains the error message
-          //If there is more than one error, browser shows only the first error
-          message: $(doc).find('parsererror div').html()
-        });
-      }
+            scxmlChangeSource.addEventListener('onChange', function(e) {
+              getScxml();
+            }, false);
+          }
 
-      if (layout) {
-        layout.unhighlightAllStates();
-
-        layout.update(doc).done(null, function(err) {
-          if (err) {
-            alert('Something went wrong: ', err.message);
-            console.log(err);
+          if(vizType === 'statechart') {
             return;
           }
-        });
-      } else {
-        vizArea.empty();
 
-        layout = new forceLayout.Layout({ // jshint ignore:line
-          kielerAlgorithm: '__klayjs',
-          parent: vizArea[0],
-          doc: doc,
-          textOnPath: false,
-          routing: 'ORTHOGONAL',
-          debug: false
-        });
-      }
+          if (!eventChangeSource) {
+            eventChangeSource = new EventSource('./_changes');
 
-      layout.initialized.then(function(err) {
-        if (err) {
-          alert('Error initializing visualization:', err.message);
-          console.log(err);
-          return;
-        }
+            eventChangeSource.addEventListener('onEntry', function(e) {
+              highlight('onEntry', e.data);
+            }, false);
 
-        layout.fit();
+            eventChangeSource.addEventListener('onExit', function(e) {
+              highlight('onExit', e.data);
+            }, false);
+          }
 
-        if(!scxmlChangeSource) {
-          scxmlChangeSource = new EventSource(statechartChangesUrl);
-
-          scxmlChangeSource.addEventListener('onChange', function(e) {
-            getScxml();
-          }, false);
-        }
-
-        if(vizType === 'statechart') {
-          return;
-        }
-
-        if (!eventChangeSource) {
-          eventChangeSource = new EventSource('./_changes');
-
-          eventChangeSource.addEventListener('onEntry', function(e) {
-            highlight('onEntry', e.data);
-          }, false);
-
-          eventChangeSource.addEventListener('onExit', function(e) {
-            highlight('onExit', e.data);
-          }, false);
-        }
-
-        $.ajax({
+          $.ajax({
             type: 'GET',
             url: './',
             dataType: 'json'
@@ -125,10 +81,44 @@ $(function() {
 
             configuration.forEach(highlight.bind(this, 'onEntry'));
           });
-      }).done();
-    } catch (e) {
-      alert('Error parsing scxml content:', e.message);
-      console.log(e);
+        }, function (err) {
+          alert(err.message);
+        });
+      });
+  }
+
+  function highlight(eventName, state) {
+    if (Array.isArray(state)) {
+      for (var eventIndex in state) {
+        layout.highlightState(state[eventIndex], eventName === 'onEntry');
+      }
+    } else {
+      layout.highlightState(state, eventName === 'onEntry');
+    }
+  }
+
+  function drawSimulation(content, onDone, onError) {
+    var doc = (new DOMParser()).parseFromString(content, 'application/xml');
+    if (doc.getElementsByTagName('parsererror').length) {
+      return onError({ message: $(doc).find('parsererror div').html() });
+    }
+
+    if (layout) {
+      layout.unhighlightAllStates();
+      layout.update(doc).then(onDone, onError);
+    } else {
+      vizArea.empty();
+
+      layout = new forceLayout.Layout({ // jshint ignore:line
+        kielerAlgorithm: '__klayjs',
+        parent: vizArea[0],
+        doc: doc,
+        textOnPath: false,
+        routing: 'ORTHOGONAL',
+        debug: false
+      });
+
+      layout.initialized.then(onDone, onError);
     }
   }
 });
