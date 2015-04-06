e8d3c5c7973954a1c7017aac55579bae56995e70
Feyzo <feyzullahdaldal@gmail.com>
2015-04-06 14:27:39 +0300

    Separated express app and api

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
 #!/usr/bin/env node
 'use strict';
 
-function init(options, cb){
+  var fs = require('fs'),
+  yaml = require('js-yaml'),
+  api = require('./providers/common/api');
 
-  var provider = options.dbProvider || require('./providers/databases/postgres-db');
-
+function initExpress (cb) {
   var express = require('express'),
-    path = require('path'),
-    fs = require('fs'),
-    yaml = require('js-yaml'),
-    app = express(),
-    api = require('./providers/common/api');
-
-  // TODO: Parameterize this so we can use npm install scxmld-docker plug-in.
-
-  var database = provider(function (err) {
-    if(err) return cb(err);
-
-    console.log('Db initialized');
-    cb(null, app, port);
-  });
-  var simulationServer = require('./providers/stateful/docker')(database);
-
-  // Initialize the api
-  api = api(simulationServer, database);
-
-  var smaasJSON = yaml.safeLoad(fs.readFileSync(__dirname + '/smaas.yml','utf8'));
-
-  var port = options.port || process.env.PORT || 8002;
-
-  smaasJSON.host = process.env.SMAAS_HOST_URL || ('localhost' + ':' + port);
+  path = require('path'),
+  app = express();
 
   // buffer the body
   app.use(function(req, res, next) {
@@ -44,63 +23,98 @@ function init(options, cb){
   app.engine('html', require('ejs').renderFile);
   app.use(express.static(path.join(__dirname, './public')));
 
-  app.get('/smaas.json', function (req, res) {
-    res.status(200).send(smaasJSON);
-  });
+  var database = require('./providers/databases/postgres-db');
+  var simulationServer = require('./providers/stateful/simple')(database);
 
-  app.get('/api/v1/:StateChartName/:InstanceId/_viz', api.instanceViz);
-  app.get('/api/v1/:StateChartName/_viz', api.statechartViz);
+  initApi({
+    app: app,
+    simulationServer: simulationServer,
+    database: database
+  }, cb);
+}
 
-  function methodNotImplementedMiddleware(req, res){
-    return res.send(501, { message: 'Not implemented' });
+function initApi(opts, cb){
+  opts = opts || {};
+  opts.basePath = opts.basePath || '/api/v1/';
+  opts.port = opts.port || process.env.PORT || 8002;
+
+  if(!opts.app || opts.database || opts.simulationServer) {
+    return cb(new Error('Missing app or database or simulationServer'));
   }
 
-  Object.keys(smaasJSON.paths).forEach(function(endpointPath){
-    var endpoint = smaasJSON.paths[endpointPath];
-    var actualPath = smaasJSON.basePath + endpointPath.replace(/{/g, ':').replace(/}/g, '');
+  opts.database(function (err) {
+    if(err) return cb(err);
 
-    Object.keys(endpoint).forEach(function(methodName){
-      var method = endpoint[methodName];
+    console.log('Db initialized');
 
-      var handler = api[method.operationId] || methodNotImplementedMiddleware;
-      switch(methodName) {
-        case 'get': {
-          app.get(actualPath, handler);
-          break;
-        }
-        case 'post': {
-          app.post(actualPath, handler);
-          break;
-        }
-        case 'put': {
-          app.put(actualPath, handler);
-          break;
-        }
-        case 'delete': {
-          app.delete(actualPath, handler);
-          break;
-        }
-        default:{
-          console.log('Unsupported method name:', methodName);
-        }
-      }
+    // Initialize the api
+    api = api(opts.simulationServer, opts.database);
+
+    var smaasJSON = yaml.safeLoad(fs.readFileSync(__dirname + '/smaas.yml','utf8'));
+    smaasJSON.host = process.env.SMAAS_HOST_URL || ('localhost' + ':' + opts.port);
+    smaasJSON.basePath = opts.basePath;
+
+    opts.app.get('/smaas.json', function (req, res) {
+      res.status(200).send(smaasJSON);
     });
-  });
 
-  app.use(function(req, res) {
-    res.status(404).send('Can\'t find ' + req.path);
+    opts.app.get(opts.basePath + ':StateChartName/:InstanceId/_viz', api.instanceViz);
+    opts.app.get(opts.basePath + ':StateChartName/_viz', api.statechartViz);
+
+    function methodNotImplementedMiddleware(req, res){
+      return res.send(501, { message: 'Not implemented' });
+    }
+
+    Object.keys(smaasJSON.paths).forEach(function(endpointPath){
+      var endpoint = smaasJSON.paths[endpointPath];
+      var actualPath = smaasJSON.basePath + endpointPath.replace(/{/g, ':').replace(/}/g, '');
+
+      Object.keys(endpoint).forEach(function(methodName){
+        var method = endpoint[methodName];
+
+        var handler = api[method.operationId] || methodNotImplementedMiddleware;
+        switch(methodName) {
+          case 'get': {
+            opts.app.get(actualPath, handler);
+            break;
+          }
+          case 'post': {
+            opts.app.post(actualPath, handler);
+            break;
+          }
+          case 'put': {
+            opts.app.put(actualPath, handler);
+            break;
+          }
+          case 'delete': {
+            opts.app.delete(actualPath, handler);
+            break;
+          }
+          default:{
+            console.log('Unsupported method name:', methodName);
+          }
+        }
+      });
+    });
+
+    opts.app.use(function(req, res) {
+      res.status(404).send('Can\'t find ' + req.path);
+    });
+
+    cb(null, opts);
   });
 }
 
 
 if(require.main === module) {
-  init(null, function(err,app, port){
-    console.log('Starting server on port:', port);
-    if(err) throw new Error('Error initializing app');
-    app.listen(port, function () {
+  initExpress(function (err, opts) {
+    console.log('Starting server on port:', opts.port);
+    if(err) throw err;
+
+    opts.app.listen(opts.port, function () {
       console.log('Server started');
     });
   });
 } else {
-  module.exports.init = init;
+  module.exports.api = api;
 }
