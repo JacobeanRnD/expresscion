5069d9dd7bec81288b5a0bbc7fbbcb3742d14fa1
Jacob Beard <jbeard4@cs.mcgill.ca>
2015-05-31 17:57:12 -0400

    Revert "Providers defaults to postgres and simple-stateless"

--- provider_list.sh
+++ provider_list.sh
@@ -2,4 +2,4 @@
 # provider_list.sh
 
 DB_PROVIDERS="SCXMLD-simple-database-provider SCXMLD-postgres-database-provider"
-SIMULATION_PROVIDERS="SCXMLD-simple-simulation-provider SCXMLD-simple-stateless-simulation-provider"
+SIMULATION_PROVIDERS="SCXMLD-simple-simulation-provider"
