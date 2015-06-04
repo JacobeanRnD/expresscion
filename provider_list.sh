ff16009027619254c52d448c515429b5617a8e4b
Jacob Beard <jbeard4@cs.mcgill.ca>
2015-06-04 19:02:18 -0400

    Modified test providers. Some failing tests

--- provider_list.sh
+++ provider_list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # provider_list.sh
 
-DB_PROVIDERS="SCXMLD-simple-database-provider SCXMLD-postgres-database-provider"
-SIMULATION_PROVIDERS="SCXMLD-simple-simulation-provider SCXMLD-simple-stateless-simulation-provider"
+DB_PROVIDERS="SCXMLD-postgres-database-provider"
+SIMULATION_PROVIDERS="SCXMLD-simple-stateless-simulation-provider"
