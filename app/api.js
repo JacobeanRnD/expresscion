dec64ee70de0cdc9fab39682dc24a70c4412b833
Jacob Beard <jbeard4@cs.mcgill.ca>
2015-06-11 09:07:58 -0400

    Fixed a bug in getInstances

--- app/api.js
+++ app/api.js
@@ -44,10 +44,6 @@ module.exports = function (simulation, db, scxmlString, modelName) {
     db.getInstances(modelName, function (err, instances) {
       if (!util.IsOk(err, res)) return;
 
-      instances = instances.map(function (id) {
-        return { id: id.split('/')[1] };
-      });
-
       res.send({ name: 'success.get.instances', data: { instances: instances }});
     });
   };
